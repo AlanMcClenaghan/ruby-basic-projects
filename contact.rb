a2c749e1925157aa45377b8291613227f58204c6
Alan <alandmcclenaghan@icloud.com>
2019-07-09 12:28:17 +0100

    Added last_first and first_last methods.

--- contact.rb
+++ contact.rb
@@ -13,9 +13,61 @@ class Contact
     @last_name
   end
 
+  def full_name
+    full_name = first_name
+    if !@middle_name.nil?
+      full_name += " "
+      full_name += middle_name
+    end
+    full_name += ' '
+    full_name += last_name
+    full_name
+  end
+
+  def last_first
+    last_first = last_name
+    last_first += ", "
+    last_first += first_name
+    if !@middle_name.nil?
+      last_first += " "
+      last_first += middle_name.slice(0, 1)
+      last_first += "."
+    end
+    last_first
+  end
+
+  def first_last
+    first_name + " " + last_name
+  end
+
+  def to_s(format = 'full_name')
+    case format
+    when 'full_name'
+      full_name
+    when 'last_first'
+      last_first
+    when 'first'
+      first_name
+    when 'last'
+      last_name
+    else
+      first_last
+    end
+  end
+
 end
 
 alan = Contact.new
 alan.first_name = "Alan"
 alan.last_name = "McClenaghan"
-puts alan.first_name + " " + alan.last_name
+puts alan.to_s
+puts alan.to_s('full_name')
+puts alan.to_s('last_first')
+
+matthew = Contact.new
+matthew.first_name = "Matthew"
+matthew.middle_name = "P"
+matthew.last_name = "McClenaghan"
+puts matthew.to_s
+puts matthew.to_s('last_first')
+puts matthew.to_s('first_last')
