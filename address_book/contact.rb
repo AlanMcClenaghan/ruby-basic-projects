85c8f243550d5a80e283def4da5d558b281c83d1
Alan <alandmcclenaghan@icloud.com>
2019-07-09 13:58:22 +0100

    Added address.rb file and class.

--- address_book/contact.rb
+++ address_book/contact.rb
@@ -1,8 +1,14 @@
 require "./phone_number"
+require "./address"
 
 class Contact
   attr_writer :first_name, :middle_name, :last_name
-  attr_reader :phone_numbers
+  attr_reader :phone_numbers, :addresses
+
+  def initialize
+    @phone_numbers = []
+    @addresses = []
+  end
 
   def first_name
     @first_name
@@ -58,10 +64,6 @@ class Contact
     end
   end
 
-  def initialize
-    @phone_numbers = []
-  end
-
   def add_phone_number(kind, number)
     phone_number = PhoneNumber.new
     phone_number.kind = kind
@@ -69,9 +71,25 @@ class Contact
     phone_numbers.push(phone_number)
   end
 
+  def add_address(kind, street_1, street_2, city, state, postal_code)
+    address = Address.new
+    address.kind = kind
+    address.street_1 = street_1
+    address.street_2 = street_2
+    address.city = city
+    address.state = state
+    address.postal_code = postal_code
+    addresses.push(address)
+  end
+
   def print_phone_numbers
     puts "Phone Numbers"
     phone_numbers.each { |phone_number| phone_number }
+  end
+
+  def print_addresses
+    puts "Addresses"
+    addresses.each { |address| address.to_s('short') }
   end
 
 end
@@ -81,9 +99,11 @@ alan.first_name = "Alan"
 alan.last_name = "McClenaghan"
 alan.add_phone_number("Home", "0208-000-000")
 alan.add_phone_number("Mobile", "07909-000-000")
+alan.add_address("Home", "123 High St", "" ,"London", "UK", "123456")
 puts alan.to_s('full_name')
 # puts alan.to_s('last_first')
 puts alan.print_phone_numbers
+puts alan.print_addresses
 
 # matthew = Contact.new
 # matthew.first_name = "Matthew"
