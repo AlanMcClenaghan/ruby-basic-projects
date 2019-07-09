14ca2f54da638b6a9f1c97e3225eea8495801085
Alan <alandmcclenaghan@icloud.com>
2019-07-09 12:00:49 +0100

    Added full_name method

--- contact.rb
+++ contact.rb
@@ -13,9 +13,25 @@ class Contact
     @last_name
   end
 
+  def full_name
+    full_name = first_name
+    if !@middle_name.nil?
+      full_name += " "
+      full_name += middle_name
+    end
+    full_name += ' '
+    full_name += last_name
+    full_name
+  end
 end
 
 alan = Contact.new
 alan.first_name = "Alan"
 alan.last_name = "McClenaghan"
 puts alan.first_name + " " + alan.last_name
+
+matthew = Contact.new
+matthew.first_name = "Matthew"
+matthew.middle_name = "P"
+matthew.last_name = "McClenaghan"
+puts matthew.full_name
