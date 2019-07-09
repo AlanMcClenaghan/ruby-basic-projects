acb1a67f189a32db9ca47654b28c3bb0e11e26e0
Alan <alandmcclenaghan@icloud.com>
2019-07-09 18:51:01 +0100

    Add contacts via the command line.

--- address_book/address_book.rb
+++ address_book/address_book.rb
@@ -15,13 +15,7 @@ class AddressBook
         results.push(contact)
       end
     end
-    puts "Name search results (#{search})"
-    results.each do |contact|
-      puts contact.to_s('full_name')
-      puts contact.print_phone_numbers
-      puts contact.print_addresses
-      puts "\n"
-    end
+    print_results("Name search results (#{search})", results)
   end
 
   def find_by_phone_number(number)
@@ -77,42 +71,52 @@ class AddressBook
     contact.middle_name = gets.chomp
     print "Last name: "
     contact.last_name = gets.chomp
+  
+    loop do
+      puts "Add phone number or address? "
+      puts "p: Add phone number"
+      puts "a: Add address"
+      puts "(Any other key to go back)"
+      response = gets.chomp.downcase
+      case response
+      when 'p'
+        phone = PhoneNumber.new
+        print "Phone number kind (Home, Work, etc): "
+        phone.kind = gets.chomp
+        print "Number: "
+        phone.number = gets.chomp
+        contact.phone_numbers.push(phone)
+      when 'a'
+        address = Address.new
+        print "Address Kind (Home, Work, etc): "
+        address.kind = gets.chomp
+        print "Address line 1: "
+        address.street_1 = gets.chomp
+        print "Address line 2: "
+        address.street_2 = gets.chomp
+        print "City: "
+        address.city = gets.chomp
+        print "State: "
+        address.state = gets.chomp
+        print "Postal Code: "
+        address.postal_code = gets.chomp
+        contact.addresses.push(address)
+      else
+        print "\n"
+        break
+      end
+    end
+  
     contacts.push(contact)
   end
   
-  def print_results(search, results)
-    puts search
-    results.each do |contact|
-      puts contact.to_s('full_name')
-      puts contact.print_phone_numbers
-      puts contact.print_addresses
-      puts "\n"
+  def print_contact_list
+    puts "Contact List"
+    contacts.each do |contact|
+      puts contact.to_s('last_first')
     end
   end
 end
 
 address_book = AddressBook.new
-
 address_book.run
-
-# alan = Contact.new
-# alan.first_name = "Alan"
-# alan.last_name = "McClenaghan"
-# alan.add_phone_number("Home", "0208-000-000")
-# alan.add_phone_number("Mobile", "07909-000-000")
-# alan.add_address("Home", "123 High St", "" ,"London", "UK", "123456")
-
-# matthew = Contact.new
-# matthew.first_name = "Matthew"
-# matthew.middle_name = "P"
-# matthew.last_name = "McClenaghan"
-# matthew.add_phone_number("Home", "0208-000-000")
-# matthew.add_address("Home", "123 High St", "" ,"London", "UK", "123456")
-
-# address_book.contacts.push(alan)
-# address_book.contacts.push(matthew)
-
-# address_book.print_contact_list
-# address_book.find_by_name("m")
-# address_book.find_by_phone_number("2")
-# address_book.find_by_address('High')
