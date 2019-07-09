41840ab1c23fcb70b27f416c2cf8710ab5029da3
Alan <alandmcclenaghan@icloud.com>
2019-07-09 18:11:50 +0100

    Adding contacts functionality.

--- address_book/address_book.rb
+++ address_book/address_book.rb
@@ -49,6 +49,36 @@ class AddressBook
     end
     print_results("Address search results (#{search})", results)
   end
+
+  def run
+    loop do
+      puts "Address Book"
+      puts "a: Add Contact"
+      puts "p: Print Address Book"
+      puts "e: Exit"
+      print "Enter your choice: "
+      input = gets.chomp.downcase
+      case input
+      when 'a'
+        add_contact
+      when 'p'
+        print_contact_list
+      when 'e'
+        break  
+      end
+    end
+  end
+
+  def add_contact
+    contact = Contact.new
+    print "First name: "
+    contact.first_name = gets.chomp
+    print "Middle name: "
+    contact.middle_name = gets.chomp
+    print "Last name: "
+    contact.last_name = gets.chomp
+    contacts.push(contact)
+  end
   
   def print_results(search, results)
     puts search
@@ -63,24 +93,26 @@ end
 
 address_book = AddressBook.new
 
-alan = Contact.new
-alan.first_name = "Alan"
-alan.last_name = "McClenaghan"
-alan.add_phone_number("Home", "0208-000-000")
-alan.add_phone_number("Mobile", "07909-000-000")
-alan.add_address("Home", "123 High St", "" ,"London", "UK", "123456")
+address_book.run
 
-matthew = Contact.new
-matthew.first_name = "Matthew"
-matthew.middle_name = "P"
-matthew.last_name = "McClenaghan"
-matthew.add_phone_number("Home", "0208-000-000")
-matthew.add_address("Home", "123 High St", "" ,"London", "UK", "123456")
+# alan = Contact.new
+# alan.first_name = "Alan"
+# alan.last_name = "McClenaghan"
+# alan.add_phone_number("Home", "0208-000-000")
+# alan.add_phone_number("Mobile", "07909-000-000")
+# alan.add_address("Home", "123 High St", "" ,"London", "UK", "123456")
 
-address_book.contacts.push(alan)
-address_book.contacts.push(matthew)
+# matthew = Contact.new
+# matthew.first_name = "Matthew"
+# matthew.middle_name = "P"
+# matthew.last_name = "McClenaghan"
+# matthew.add_phone_number("Home", "0208-000-000")
+# matthew.add_address("Home", "123 High St", "" ,"London", "UK", "123456")
+
+# address_book.contacts.push(alan)
+# address_book.contacts.push(matthew)
 
 # address_book.print_contact_list
 # address_book.find_by_name("m")
 # address_book.find_by_phone_number("2")
-address_book.find_by_address('High')
+# address_book.find_by_address('High')
